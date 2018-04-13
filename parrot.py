322b9ecbc5c72b78c8518978d525c865c9093333
Frederick <trent.frederick@WAYNE.COM>
2018-04-13 10:45:36 -0500

    added groups to parrot bot code. Ready for GTBz group deployment

--- parrot.py
+++ parrot.py
@@ -22,7 +22,13 @@ def post():
 	msg = data['text']
 	if '@coffee' in msg:
 		at_all(bot, group)
-	
+	for y in peeps.keys():
+                if y in msg:
+                        key = y
+                        skwak(bot, key)
+	if '@parrot' in msg and 'tags' in message:
+                tags(bot)
+                
 	return "ok", 200
 
 def at_all(bot, group):
@@ -50,4 +56,34 @@ def at_all(bot, group):
 	tag = attachments.Mentions(mention['loci'], mention['user_ids'])
 
 	client.bots.post(bot, text, attachments = [tag])
+
+def skwak(bot, key):
+        user_ids = peeps[key]
+	loci = []
+	text = ""
+	pnt = 0
+
+	for m in user_ids:
+		name = "@" + m.data["nickname"] + " "
+                n = [pnt, len(name)]
+		loci.append(n) 
+		pnt += len(name)
+		text += name
+
+	mention = {}
+	mention["user_ids"] = user_ids
+	mention["loci"] = loci
+	tag = attachments.Mentions(mention['loci'], mention['user_ids'])
+	client.bots.post(bot, text, attachments = [tag])
+
+def tags(bot):
+        text = ""
+        for m in peeps.keys():
+                text += m + ' '
+        client.bots.post(bot, text)
+
+peeps = {'@skwad':['482066', '2513725', '36741', '2513723', '36739'],
+         '@frolf':['482066', '8206212', '2513726', '36739', '36740', '30472260', '30685722'],
+         '@games':['482066', '8206212', '2513726', '6698773', '30472260', '8206213', '34951757']}
+
 
